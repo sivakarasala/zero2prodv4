6bb9719679261d2c9f3c8cb89b3a3f27426e8a7f
Siva Karasala <shivataraya@gmail.com>
2025-10-06 13:45:51 +0530

    feat: add health-check endpoint

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+use std::net::TcpListener;
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prodv4::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind random port");
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener)?.await
 }
